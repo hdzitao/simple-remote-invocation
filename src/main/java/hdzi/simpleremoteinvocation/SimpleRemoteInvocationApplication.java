8c851ab08cae1c26e58b4001f20f501d7b1ad66c
taojinhou <taojinhou@qq.com>
2021-12-02 20:45:00 +0800

    初始化

---
+++ src/main/java/hdzi/simpleremoteinvocation/SimpleRemoteInvocationApplication.java
@@ -0,0 +1,13 @@
+package hdzi.simpleremoteinvocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRemoteInvocationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleRemoteInvocationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hdzi/simpleremoteinvocation/SimpleRemoteInvocationApplicationTests.java
@@ -0,0 +1,14 @@
+package hdzi.simpleremoteinvocation;
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.StringUtils;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRemoteInvocationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
