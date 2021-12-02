ff815f2d509ec1dde8aa09ff8e18b1d492fb4c29
taojinhou <taojinhou@qq.com>
2021-12-02 21:24:10 +0800

    格式化

--- src/test/java/hdzi/simpleremoteinvocation/SimpleRemoteInvocationApplicationTests.java
+++ src/test/java/hdzi/simpleremoteinvocation/SimpleRemoteInvocationApplicationTests.java
@@ -1,7 +1,6 @@
 package hdzi.simpleremoteinvocation;
 
 import org.junit.jupiter.api.Test;
-import org.junit.platform.commons.util.StringUtils;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
